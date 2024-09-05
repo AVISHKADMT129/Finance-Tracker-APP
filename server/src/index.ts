645ced9c445e8b2cca50f3466de4be1b6d2725b9
Avishka-Weeraddana <weeraddanaar.21@uom.lk>
2024-09-05 20:33:00 +0530

    Initial commit

---
+++ server/src/index.ts
@@ -0,0 +1,25 @@
+//PWD=Gw148jMwFpJV7vTW
+import express, { Express } from "express";
+import mongoose from "mongoose";
+import financialRecordRouter from "./routes/financial-records";
+// import cors from "cors";
+
+const app: Express = express();
+const port = process.env.PORT || 3001;
+
+app.use(express.json());
+// app.use(cors());
+
+const mongoURI: string =
+  "mongodb+srv://avishka1999r:Gw148jMwFpJV7vTW@personalfinancetracker.jhbof.mongodb.net/";
+ 
+mongoose
+  .connect(mongoURI)
+  .then(() => console.log("CONNECTED TO MONGODB!"))
+  .catch((err) => console.error("Failed to Connect to MongoDB:", err));
+
+app.use("/financial-records", financialRecordRouter);
+
+app.listen(port, () => {
+  console.log(`Server Running on Port ${port}`);
+});
