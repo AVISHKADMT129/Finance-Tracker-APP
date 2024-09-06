6a355cf0c8670d037a5758fab1b613492fe6d57a
Avishka-Weeraddana <weeraddanaar.21@uom.lk>
2024-09-06 21:17:49 +0530

    Commit

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,13 +2,13 @@
 import express, { Express } from "express";
 import mongoose from "mongoose";
 import financialRecordRouter from "./routes/financial-records";
-// import cors from "cors";
+import cors from "cors";
 
 const app: Express = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 
 const mongoURI: string =
   "mongodb+srv://avishka1999r:Gw148jMwFpJV7vTW@personalfinancetracker.jhbof.mongodb.net/";
